874cf5c445e9247344d7d5e77461545ada8bdd31
Brookzerker <brooks@brookzerker.com>
2023-10-13 15:01:20 -0600

    set up for taking assessment

--- src/authors.rs
+++ src/authors.rs
@@ -1,156 +1,69 @@
-use crate::books::{Book, BookAuthor, BookId};
-use eyre::{bail, Result};
+use crate::books::{Book, BookId};
+use eyre::Result;
 use sqlx::{Pool, Postgres};
 use std::collections::HashMap;
 
 pub type AuthorId = i32;
 
-pub async fn create_author(pool: &Pool<Postgres>, name: &str) -> Result<AuthorId> {
-    let result = sqlx::query!(
-        "INSERT INTO authors (name) VALUES ($1) RETURNING author_id",
-        name
-    )
-    .fetch_one(pool)
-    .await?;
-
-    Ok(result.author_id)
+/// Insert an author into the database and return the newly created author id
+pub async fn create_author(_pool: &Pool<Postgres>, _name: &str) -> Result<AuthorId> {
+    todo!()
 }
 
-pub async fn get_author_by_id(pool: &Pool<Postgres>, id: i32) -> Result<Option<Author>> {
-    let author = sqlx::query_as!(Author, "SELECT * FROM authors WHERE author_id = $1", id)
-        .fetch_optional(pool)
-        .await?;
-
-    Ok(author)
+/// Retrieve a single author from the database and return it. We don't care about their books yet so this will just be an object that has the author id and the name.
+///
+/// Since it's possible that the id doesn't exist in the database, return a None if the author cannot be found.
+pub async fn get_author_by_id(_pool: &Pool<Postgres>, _id: AuthorId) -> Result<Option<Author>> {
+    todo!()
 }
 
-pub async fn get_all_authors(pool: &Pool<Postgres>) -> Result<Vec<Author>> {
-    let authors = sqlx::query_as!(Author, "SELECT * FROM authors")
-        .fetch_all(pool)
-        .await?;
-
-    Ok(authors)
+/// Retrieve all of the authors from the database and return them. We don't care about their books yet so this will just be a Vector of objects
+pub async fn get_all_authors(_pool: &Pool<Postgres>) -> Result<Vec<Author>> {
+    todo!()
 }
 
-pub async fn update_author(pool: &Pool<Postgres>, id: i32, name: &str) -> Result<()> {
-    sqlx::query!(
-        "UPDATE authors SET name = $1 WHERE author_id = $2",
-        name,
-        id
-    )
-    .execute(pool)
-    .await?;
-
-    Ok(())
+/// Update the author's name in the database.
+pub async fn update_author(_pool: &Pool<Postgres>, _id: AuthorId, _name: &str) -> Result<()> {
+    todo!()
 }
 
-pub async fn delete_author(pool: &Pool<Postgres>, id: i32) -> Result<()> {
-    sqlx::query!("DELETE FROM authors WHERE author_id = $1", id)
-        .execute(pool)
-        .await?;
-
-    Ok(())
+/// Permanently delete the author from the database.
+pub async fn delete_author(_pool: &Pool<Postgres>, _id: AuthorId) -> Result<()> {
+    todo!()
 }
 
+/// Create an author, a book, and associate them together all at the same time in the database.
+///
+/// This is bulk operation, ensure that if any of the commands fail, then the database will not be changed.
+///
+/// Return a tuple with the author id and book ids that are created
 pub async fn create_author_and_book(
-    pool: &Pool<Postgres>,
-    author_name: &str,
-    book_name: &str,
+    _pool: &Pool<Postgres>,
+    _author_name: &str,
+    _book_name: &str,
 ) -> Result<(AuthorId, BookId)> {
-    let mut transaction = pool.begin().await?;
-
-    let author_id = match sqlx::query!(
-        "INSERT INTO authors (name) VALUES ($1) RETURNING author_id",
-        author_name
-    )
-    .fetch_one(&mut *transaction)
-    .await
-    {
-        Ok(result) => result.author_id,
-        Err(error) => {
-            eprintln!("Error inserting author: {error}");
-            transaction.rollback().await?;
-            bail!("There was a problem inserting the author");
-        }
-    };
-
-    let book_id = match sqlx::query!(
-        "INSERT INTO books (name) VALUES ($1) RETURNING book_id",
-        book_name
-    )
-    .fetch_one(&mut *transaction)
-    .await
-    {
-        Ok(result) => result.book_id,
-        Err(error) => {
-            eprintln!("Error inserting book: {error}");
-            transaction.rollback().await?;
-            bail!("There was a problem inserting the book");
-        }
-    };
-
-    if let Err(error) = sqlx::query!(
-        "INSERT INTO book_authors (author_id, book_id) VALUES ($1, $2)",
-        author_id,
-        book_id
-    )
-    .execute(&mut *transaction)
-    .await
-    {
-        eprintln!("Error inserting book_author: {error}");
-        transaction.rollback().await?;
-        bail!("There was a problem associating the author and book");
-    }
-
-    transaction.commit().await?;
-
-    Ok((author_id, book_id))
+    todo!()
 }
 
+/// When returning all of the authors together implement a HashMap as provided here or any other Maps, for example a BTreeMap if you want to ensure the order of the authors in the Map.
+///
+/// The author should have the books associated them in a Vector
 pub type Authors = HashMap<AuthorId, AuthorWithBooks>;
 
-pub async fn get_all_authors_with_books(pool: &Pool<Postgres>) -> Result<Authors> {
-    let book_authors = sqlx::query_as!(
-        BookAuthor,
-        r#"
-            SELECT
-                a.author_id,
-                a.name AS author_name,
-                b.book_id,
-                b.name AS book_name
-            FROM book_authors ba
-            JOIN authors a ON a.author_id = ba.author_id
-            JOIN books b ON b.book_id = ba.book_id
-        "#
-    )
-    .fetch_all(pool)
-    .await?;
-
-    let mut authors = Authors::new();
-
-    for book_author in book_authors {
-        let author = authors
-            .entry(book_author.author_id)
-            .or_insert(AuthorWithBooks {
-                author_id: book_author.author_id,
-                name: book_author.author_name,
-                books: vec![],
-            });
-
-        author.books.push(Book {
-            book_id: book_author.book_id,
-            name: book_author.book_name,
-        });
-    }
-
-    Ok(authors)
+/// Retrieve all of the authors from the database with their books. Use a single query to the database to get all of the data you need and then return the authors using the Authors type.
+pub async fn get_all_authors_with_books(_pool: &Pool<Postgres>) -> Result<Authors> {
+    todo!()
 }
 
+/// Single Author with just it's id and name
+///
+/// We don't care about the Author's books yet
 pub struct Author {
     pub author_id: AuthorId,
     pub name: String,
 }
 
+/// Single Author with their books in a Vector. the books are the simgle single Book type which just includes the book id and name.
 pub struct AuthorWithBooks {
     pub author_id: AuthorId,
     pub name: String,
